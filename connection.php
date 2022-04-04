74a9b6ec2c2b64ac9574a7d7096dfd0222db0e99
Douniaawane <102249747+Douniaawane@users.noreply.github.com>
2022-04-04 01:08:57 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,12 +1,9 @@
 <?php
-$con = mysqli_connect("localhost","root","root","gestion_etudiants_db");
+$con = mysqli_connect("localhost","root","","gestion_etudiants_db");
 if(mysqli_connect_errno()){
 echo "connection echec :";
 
-
-
-
-
-}
+}else
+echo "succes";
 
 ?>
